5118d33e0b9c96136c3dc84578b2e39ab48585c0
ilyoup kwak <ikwak@umn.edu>
2017-07-15 21:46:18 +0900

    fix bugs

--- R/DrImpute.R
+++ R/DrImpute.R
@@ -28,9 +28,9 @@
 #'
 #' data(exdata)
 #' exdata <- preprocessSC(exdata)
+#' exdata <- exdata[1:3000, 1:80]
 #' logdat <- log(exdata+1)
 #' cls <- getCls(logdat)
-#' 
 #' logdat_imp <- DrImpute(logdat, cls = cls)
 #' 
 
--- R/getCls.R
+++ R/getCls.R
@@ -22,6 +22,7 @@
 #'
 #' data(exdata)
 #' exdata <- preprocessSC(exdata)
+#' exdata <- exdata[1:3000, 1:80]
 #' logdat <- log(exdata+1)
 #' cls <- getCls(logdat)
 #'
